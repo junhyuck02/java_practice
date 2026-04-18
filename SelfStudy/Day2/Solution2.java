95f6556a5c2461c36785a5852c2e0c125b661825
최준혁 <ryan020820@gmail.com>
2026-04-18 19:07:44 +0900

    Create Solution2.java

---
+++ SelfStudy/Day2/Solution2.java
@@ -0,0 +1,38 @@
+package SelfStudy.Day2;
+
+import java.util.Scanner;
+
+class Solution2 {
+    public int solution(int num1, int num2) {
+        if(num1 == num2){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner  = new Scanner(System.in); 
+        int num1, num2, result = 0;
+
+        while (true) {
+            System.out.print("두 개의 정수를 입력하세요 (0 ~ 10000): ");
+            num1 = scanner.nextInt();
+            num2 = scanner.nextInt();
+
+            if (num1 >= 0 && num1 <= 10000 && num2 >= 0 && num2 <= 10000) {
+                break;
+            }
+            System.out.println("다시 입력하세요.");
+        }
+            
+        Solution2 s = new Solution2();
+        result = s.solution(num1, num2);
+        System.out.println(result);
+        scanner.close();
+
+    }
+
+
+}
