11d1e075ff768884190b0f04dab61af8bc011672
최준혁 <ryan020820@gmail.com>
2026-04-19 15:44:22 +0900

    Update Solution3.java

--- SelfStudy/Day3/Solution3.java
+++ SelfStudy/Day3/Solution3.java
@@ -1,5 +1,44 @@
 package SelfStudy.Day3;
 
-public class Solution3 {
+import java.util.Scanner;
 
+public class Solution3 {
+    public int solution(int[] array) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {};
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("배열의 크기를 입력하세요: ");
+            int size = scanner.nextInt();
+
+            if(size > 0 && size < 100){
+                numbers = new int[size];
+                for(int i = 0; i < numbers.length; i++){
+                    System.out.print("배열의 요소를 입력하세요: ");
+                    int check = scanner.nextInt();
+
+                    if(check >= 0 && check < 1000){
+                        numbers[i] = check;
+                    } else {
+                        System.out.println("다시 입력하세요");
+                        break;
+                    }
+                    
+                }
+                break;
+                    
+            } else {
+                System.out.println("다시 입력하세요");
+            }
+            
+        }
+        int result = new Solution3().solution(numbers);
+        System.out.println("최빈값은: " + result + "입니다.");
+        scanner.close();
+    }
 }
