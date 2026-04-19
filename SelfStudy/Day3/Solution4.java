a87eedbfd4a08ec5219ad3c2f43520fdb86cd417
최준혁 <ryan020820@gmail.com>
2026-04-19 16:34:21 +0900

    Update Solution4.java

--- SelfStudy/Day3/Solution4.java
+++ SelfStudy/Day3/Solution4.java
@@ -1,5 +1,39 @@
 package SelfStudy.Day3;
 
-public class Solution4 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Solution4 {
+    public int[] solution(int n) {
+        int[] answer = {};
+        int num = 1;
+        if(n % 2 == 0){
+            answer = new int[n/2];
+        } else{
+            answer = new int[n/2 + 1];
+        }
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = num;
+            num += 2;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {};
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("숫자를 입력하세요: ");
+            int num = scanner.nextInt();
+            if(num >= 1 && num <= 100){
+                numbers = new Solution4().solution(num);
+                break;
+            } else {
+                System.out.println("다시 입력하세요");
+            }   
+        }
+        System.out.println(Arrays.toString(numbers));
+        scanner.close();
+    }
 }
