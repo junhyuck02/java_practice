8eb3e494976ba60e9c2bb34e0fab9d7d3894d5ac
최준혁 <ryan020820@gmail.com>
2026-04-19 15:40:18 +0900

    Update Solution2.java

--- SelfStudy/Day3/Solution2.java
+++ SelfStudy/Day3/Solution2.java
@@ -1,9 +1,47 @@
 package SelfStudy.Day3;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Solution2 {
     public int solution(int[] array) {
         int answer = 0;
+        Arrays.sort(array);
+        answer = array[array.length / 2];
         return answer;
     }
-    
+    public static void main(String[] args) {
+        int numbers[] = {};
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("배열의 크기를 입력하세요: ");
+            int size = scanner.nextInt();
+
+            if(size > 0 && size < 100 && (size % 2 == 1)){
+                numbers = new int[size];
+                for(int i = 0; i < numbers.length; i++){
+                    System.out.print(("배열의 요소를 입력하세요: "));
+                    int check = scanner.nextInt();
+
+                    if(check > -1000 && check < 1000){
+                        numbers[i] = check;
+                    } else {
+                        System.out.println("다시 입력하세요");
+                        break;
+                    }
+                    
+                }
+                break;
+                    
+            } else {
+                System.out.println("다시 입력하세요");
+            }
+            
+        }
+        int result = new Solution2().solution(numbers);
+        System.out.println("중앙값은: " + result + "입니다.");
+        scanner.close();
+    }
 }
