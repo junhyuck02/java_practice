4589705a284ac61ae32f12aa86d1ddc074d32895
최준혁 <ryan020820@gmail.com>
2026-04-20 22:52:57 +0900

    Update Solution2.java

--- SelfStudy/Day5/Solution2.java
+++ SelfStudy/Day5/Solution2.java
@@ -1,5 +1,35 @@
 package SelfStudy.Day5;
 
-public class Solution2 {
+import java.util.Scanner;
 
+public class Solution2 {
+    public int[] solution(int money) {
+        int[] answer = {};
+        int count = money / 5500;
+        int change = money % 5500;
+        answer = new int[2];
+        answer[0] = count;
+        answer[1] = change;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int money = 0;
+        int arr[] = new int[2];
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("돈을 입력하세요: ");
+            money = scanner.nextInt();
+            if (money > 0 && money <= 1000000) {
+                arr =  new Solution2().solution(money);
+                break;
+            } else{
+                System.out.println("다시 입력하세요.");
+                continue;
+            }
+        }
+        System.out.println("[" + arr[0] + ", " + arr[1] + "]");
+        scanner.close();
+    }
 }
