fbf5f7e54d862e4ed9960ed89753b09913dde61c
최준혁 <ryan020820@gmail.com>
2026-04-20 22:27:53 +0900

    Update Solution4.java

--- SelfStudy/Day4/Solution4.java
+++ SelfStudy/Day4/Solution4.java
@@ -24,32 +24,35 @@ public class Solution4 {
 
             if(size >= 1 && size <= 100){
                 numbers = new int[size];
+
                 for(int i = 0; i < numbers.length; i++){
                     System.out.print("배열의 요소를 입력하세요: ");
                     int check = scanner.nextInt();
 
                     if(check >= 0 && check <= 1000){
                         numbers[i] = check;
-
-                        int sum = 0;
-                        for(int j = 0; j < numbers.length; j++){
-                            sum += numbers[j];
-                        }
-                        if(sum % size == 0 || sum % size == size / 2){
-                            double average = new Solution4().solution(numbers);
-                            System.out.println("평균값은: " + average + "입니다.");
-                        } else{
-                            System.out.println("다시 입력하세요");
-                        }
-
                     } else {
-                        System.out.println("다시 입력하세요");
+                        System.out.println("다시 입력하세요.");
+                        i--;
                     }
                 }
-                break;
+
+                int sum = 0;
+                for(int num : numbers){
+                    sum += num;
+                }
+                if(sum % size == 0 || sum % size == size / 2) {
+                    double average = new Solution4().solution(numbers);
+                    System.out.println("평균값은: " + average + "입니다.");
+                    break;
+                } else {
+                    System.out.println("평균이 .5나 .0으로 끝나지 않습니다. 처음부터 다시 시작하세요.");
+                    break;
+                }
                     
             } else {
                 System.out.println("다시 입력하세요");
+                continue;
             }
             
         }
