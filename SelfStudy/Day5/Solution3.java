33249ae3729dd1e36157f5734a3d7d3f89ab22ff
최준혁 <ryan020820@gmail.com>
2026-04-20 23:11:20 +0900

    Update Solution3.java

--- SelfStudy/Day5/Solution3.java
+++ SelfStudy/Day5/Solution3.java
@@ -1,5 +1,29 @@
 package SelfStudy.Day5;
 
-public class Solution3 {
+import java.util.Scanner;
 
+public class Solution3 {
+    public int solution(int age) {
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+    public static void main(String[] args) {
+        int age = 0, year = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("나이를 입력하세요: ");
+            age = scanner.nextInt();
+
+            if(age <= 120  && age > 0){
+                year = new Solution3().solution(age);
+                break;
+            } else {
+                System.out.println("다시 입력하세요.");
+                continue;
+            }
+        }
+        System.out.println("년도는 " + year + " 입니다.");
+        scanner.close();
+    }
 }
