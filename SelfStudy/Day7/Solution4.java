8f5e61aa141b88335c2ece665dcad45840127880
최준혁 <ryan020820@gmail.com>
2026-04-21 19:45:56 +0900

    Update Solution4.java

--- SelfStudy/Day7/Solution4.java
+++ SelfStudy/Day7/Solution4.java
@@ -1,5 +1,33 @@
 package SelfStudy.Day7;
 
-public class Solution4 {
+import java.util.Scanner;
 
+public class Solution4 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int num = 0, total = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("n 을 입력하세요: ");
+            num = scanner.nextInt();
+            if (num > 0 && num <= 1000) {
+                total = new Solution4().solution(num);
+                System.out.println("n 이하의 짝수의 합계는 " + total + "입니다.");
+                break;
+            } else{
+                System.out.println("다시 입력하세요.");
+                continue;
+            }
+        }
+        scanner.close();
+
+    }
 }
