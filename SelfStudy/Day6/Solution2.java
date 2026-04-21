c3040afbc83b2878cc658dc97bf38345a1d1424e
최준혁 <ryan020820@gmail.com>
2026-04-21 16:57:16 +0900

    Update Solution2.java

--- SelfStudy/Day6/Solution2.java
+++ SelfStudy/Day6/Solution2.java
@@ -1,5 +1,28 @@
 package SelfStudy.Day6;
 
-public class Solution2 {
+import java.util.Scanner;
 
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            int n = scanner.nextInt();
+            
+            if(n >= 1 && n <= 10){
+                for(int i = 1; i <= n; i++){ 
+                    for(int j = 0; j < i; j++){ 
+                        System.out.print("*");
+                    }
+                    System.out.println(); 
+                }
+                break;
+            } else{
+                System.out.println("다시 입력하세요.");
+                continue;
+            }
+            
+        }
+        scanner.close();
+    }
 }
