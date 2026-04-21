ca104bff157f5fe3c9d24ad4d6edd73292f623e3
최준혁 <ryan020820@gmail.com>
2026-04-21 18:11:16 +0900

    Update Solution4.java

--- SelfStudy/Day6/Solution4.java
+++ SelfStudy/Day6/Solution4.java
@@ -1,5 +1,44 @@
 package SelfStudy.Day6;
 
-public class Solution4 {
+import java.util.Scanner;
 
+public class Solution4 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        for(int i = 0; i < arr.length; i++){
+            String s = arr[i];
+            answer += s.repeat(n);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        String s = "";
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("문자열을 입력하세요: ");
+            s = scanner.nextLine();
+            if(s.length() >= 2 && s.length() <= 5){
+                while(true){
+                    System.out.print("반복할 횟수를 입력하세요: ");
+                    n = scanner.nextInt();
+                    if(n >= 2 && n <= 10){
+                        String result = new Solution4().solution(s, n);
+                        System.out.println(result);
+                        break;
+                    } else{
+                        System.out.println("다시 입력하세요");
+                    }
+                }
+                break;
+            }
+            else{
+                System.out.println("다시 입력하세요");
+                continue;
+            }
+        }
+        scanner.close();
+    }
 }
