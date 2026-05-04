59fb4f568a8d9cb7b5f1629813407eca2f1d95e7
최준혁 <ryan020820@gmail.com>
2026-05-04 20:17:47 +0900

    Update Solution2.java

--- SelfStudy/Day8/Solution2.java
+++ SelfStudy/Day8/Solution2.java
@@ -1,8 +1,32 @@
 package SelfStudy.Day8;
 
+import java.util.Scanner;
+
 public class Solution2 {
     public String solution(int age) {
-        String answer = "";
-        return answer;
+        String result = "";
+        for (char check : String.valueOf(age).toCharArray()) {
+            result += (char)('a' + (check - '0'));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Solution2 sol = new Solution2();
+
+        int age = 0;
+        while (true) {
+            System.out.print("나이 입력: ");
+            age = scanner.nextInt();
+            if (age >= 1 && age <= 1000){
+                break;
+            }
+            System.out.println("다시 입력하세요.");
+        }
+
+        System.out.println(sol.solution(age));
+
+        scanner.close();
     }
 }
