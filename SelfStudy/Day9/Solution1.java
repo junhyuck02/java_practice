6819f31e734ba7eef13af6b4025f9a9d0b3c737c
최준혁 <ryan020820@gmail.com>
2026-05-09 17:30:37 +0900

    Update Solution1.java

--- SelfStudy/Day9/Solution1.java
+++ SelfStudy/Day9/Solution1.java
@@ -1,10 +1,32 @@
 package SelfStudy.Day9;
 
+import java.util.Scanner;
+
 public class Solution1 {
 
-
+    public int solution(int hp) {
+        int num1 = hp / 5;
+        int num2 = (hp % 5) / 3;
+        int num3 = (hp % 5) % 3;
+        return num1 + num2 + num3;
+    }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Solution1 sol = new Solution1();
+
+        while(true){
+            System.out.print("hp를 입력하세요:");
+            int hp = scanner.nextInt();
+
+            if(hp <= 0 || hp > 1000){
+                System.out.println("다시 입력하세요.");
+            } else {
+                System.out.println(sol.solution(hp));
+                break;
+            }
+        }
+        scanner.close();
         
     }
 }
