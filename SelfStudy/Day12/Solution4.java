5e74c958424f0eddccd43cdb8fa0b6a44174191a
최준혁 <ryan020820@gmail.com>
2026-05-13 20:17:03 +0900

    Update Solution4.java

--- SelfStudy/Day12/Solution4.java
+++ SelfStudy/Day12/Solution4.java
@@ -1,5 +1,50 @@
 package SelfStudy.Day12;
 
-public class Solution4 {
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class Solution4 {
+    public int[] solution(int n) {
+        int[] answer = {};
+        Set<Integer> set = new LinkedHashSet<>();
+        int divisor = 2;
+
+        while(n > 1){
+            if(n % divisor == 0){
+                set.add(divisor);
+                n = n / divisor;
+            }else{
+                divisor++;
+            }
+        }
+        answer = new int[set.size()];
+        int index = 0;
+        for(int num : set){
+            answer[index++] = num;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Solution4 sol = new Solution4();
+        int n = 0;
+        int[] result = {};
+
+        while(true){
+            System.out.print("n을 입력하세요:");
+            n = scanner.nextInt();
+            if(n >= 2 && n <= 10000){
+                result = sol.solution(n);
+                System.out.println(Arrays.toString(result));
+                break;
+            } else{
+                System.out.println("다시 입력하세요.");
+            }
+        }
+        scanner.close();
+
+    }
 }
