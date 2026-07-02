531d1598c128b0f3b7e585d1b2a9ad431c2b31ca
최준혁 <ryan020820@gmail.com>
2026-07-02 13:57:48 +0900

    .

--- SelfStudy/Day16/Solution1.java
+++ SelfStudy/Day16/Solution1.java
@@ -1,5 +1,13 @@
 package SelfStudy.Day16;
 
 public class Solution1 {
-
+    public int solution(String message) {
+        int answer = 0;
+        String[] arr = message.split("");
+        for (int i = 0; i < arr.length; i++) {
+            answer++;
+        }
+        return answer * 2;
+        // 초간단: eturn message.length()*2;
+    }
 }
--- SelfStudy/Day16/Solution3.java
+++ SelfStudy/Day16/Solution3.java
@@ -1,5 +1,17 @@
 package SelfStudy.Day16;
 
 public class Solution3 {
-
+    public int solution(String my_string) {
+        String[] arr = my_string.split(" ");
+        // 공백으로 끊어서 10의자리 숫자 이상 예방
+        int num = Integer.parseInt(arr[0]);
+        for (int i = 1; i < arr.length; i += 2) {
+            if (arr[i].equals("+")) {
+                num += Integer.parseInt(arr[i + 1]);
+            } else if (arr[i].equals("-")) {
+                num -= Integer.parseInt(arr[i + 1]);
+            }
+        }
+        return num;
+    }
 }
