2affb1dc99c6951bb88e3c8551dcdf9d1d8bb33f
최준혁 <ryan020820@gmail.com>
2026-07-16 16:15:32 +0900

    .

--- Step1/Day19/Solution1R.java
+++ Step1/Day19/Solution1R.java
@@ -1,12 +1,14 @@
 package Step1.Day19;
 
-public class Solution1 {
+public class Solution1R {
     public int solution(int[] array) {
         int answer = 0;
         for (int i = 0; i < array.length; i++) {
             String str = String.valueOf(array[i]);
-            if (str.contains("7")) {
-                answer++;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == '7') {
+                    answer++;
+                }
             }
         }
         return answer;
--- Step1/Day19/Solution2.java
+++
@@ -1,8 +0,0 @@
-package Step1.Day19;
-
-public class Solution2 {
-    public String[] solution(String my_str, int n) {
-        String[] answer = {};
-        return answer;
-    }
-}
