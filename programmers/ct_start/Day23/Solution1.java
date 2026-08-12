77aca672d8700e30554ef23c81c6d053fc62cd8a
junhyuck02 <ryan020820@gmail.com>
2026-08-12 16:05:08 +0900

    Update Solution1.java

--- programmers/ct_start/Day23/Solution1.java
+++ programmers/ct_start/Day23/Solution1.java
@@ -1,5 +1,12 @@
 package programmers.ct_start.Day23;
 
 public class Solution1 {
-
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = {};
+        int[] check = new int[numlist.length];
+        for (int i = 0; i < numlist.length; i++) {
+            check[i] = Math.abs(numlist[i] - n);
+        }
+        return answer;
+    }
 }
