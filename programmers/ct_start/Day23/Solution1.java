9fe9f18bcab758aee8b3eff945b211f7cd81a6bb
junhyuck02 <ryan020820@gmail.com>
2026-08-15 16:02:09 +0900

    Update Solution1.java

--- programmers/ct_start/Day23/Solution1.java
+++ programmers/ct_start/Day23/Solution1.java
@@ -1,12 +1,17 @@
 package programmers.ct_start.Day23;
 
+import java.util.Arrays;
+
 public class Solution1 {
     public int[] solution(int[] numlist, int n) {
-        int[] answer = {};
-        int[] check = new int[numlist.length];
-        for (int i = 0; i < numlist.length; i++) {
-            check[i] = Math.abs(numlist[i] - n);
-        }
-        return answer;
+        return Arrays.stream(numlist).boxed().sorted((a, b) -> {
+            int distA = Math.abs(a - n);
+            int distB = Math.abs(b - n);
+            if (distA != distB) {
+                return distA - distB;
+            }
+            return b - a;
+        }).mapToInt(Integer::intValue).toArray();
     }
+    // 이해 못함
 }
