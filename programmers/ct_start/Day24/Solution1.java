bed72c186255b4a5a8daafcff7a7a8eef11b35df
junhyuck02 <ryan020820@gmail.com>
2026-08-30 17:15:12 +0900

    .

--- programmers/ct_start/Day24/Solution1.java
+++
@@ -1,5 +0,0 @@
-package programmers.ct_start.Day24;
-
-public class Solution1 {
-
-}
---
+++ programmers/ct_start/Day24/Solution1R.java
@@ -0,0 +1,16 @@
+package programmers.ct_start.Day24;
+
+public class Solution1R {
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = chicken;
+
+        while (coupon >= 10) {
+            int service = coupon / 10;
+            answer += service;
+            coupon = coupon % 10 + service;
+        }
+
+        return answer;
+    }
+}
